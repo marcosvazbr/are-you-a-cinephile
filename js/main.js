e9830cc879ef9da97d81e7678bc924b9bc4fbdf3
marcosvazbr <mvg.vaz@gmail.com>
2023-03-27 20:00:18 -0300

    finalizando o trabalho

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let inputSearch = document.querySelector('#searchInput')
-let buttonSearch = document.querySelector('#searchButton')
-
+let yourScoreButton = document.querySelector('.button-score')
+let yourScore = document.querySelector('.score')
+let yourFilmsScore = []
 
 inputSearch.addEventListener('keyup', function(e) {
   console.log(e.key)
@@ -12,12 +13,6 @@ inputSearch.addEventListener('keyup', function(e) {
   }
   console.log(inputSearch.value)
   
-})
-
-buttonSearch.addEventListener('click', function() {
-  let nameMovie = inputSearch.value.replaceAll(" ", "+")
-
-  movieApi(nameMovie)
 })
 
 function movieApi(name) {
@@ -55,26 +50,64 @@ function movieApi(name) {
 
         
         let filmTr = document.createElement('tr')
-        filmTr.innerHTML = `<td id="name-movie">
-        <div id="img-movie">
-          <img src="https://image.tmdb.org/t/p/w500/${filme.poster_path}" alt="Capa do filme">
-        </div>
-        <div>
-          <a href="https://www.themoviedb.org/movie/${filme.id}" target="_blank">${filme.original_title} 
-            <p>${filme.title}</p>
-          </a>
+        filmTr.classList.add('table-body')
+        filmTr.innerHTML = `
+        <td id="name-movie" >
+          <div id="img-movie" >
+            <img src="https://image.tmdb.org/t/p/w500/${filme.poster_path}" alt="Capa do filme">
+          </div>
+          <div>
+            <a href="https://www.themoviedb.org/movie/${filme.id}" target="_blank">${filme.original_title} 
+              <p>${filme.title}</p>
+            </a>
           
-        </div>
-      </td> 
-      <td id="typeMovie">${filme.release_date}</td>
-          <td id="yearMovie">${filme.vote_count}</td>
-          <td id="scoreMovie">${filme.vote_average}</td>`
+          </div>
+        </td> 
+        <td id="typeMovie" >${filme.release_date}</td>
+        <td id="yearMovie" >${filme.vote_count}</td>
+        <td id="scoreMovie" >${filme.vote_average}</td>`
         
 
           document.querySelector('tbody').append(filmTr)
         
-
+      yourFilmsScore.push(filme.vote_average)
+      
       }
+
+      yourScoreButton.addEventListener('click', () => {
+        let score = 0
+        if(yourFilmsScore.length <= 2){
+          alert("You need more films in your list!!!")
+          return
+        } else { 
+          for(let i=0; i <= yourFilmsScore.length - 1; i++) {
+            score = score + yourFilmsScore[i]
+          }
+          score = score / yourFilmsScore.length
+          yourScore.innerText = score.toFixed(2)
+          yourScoreButton.classList.add('hide')
+          yourScore.classList.remove('hide')
+
+          console.log(Math.round(score))
+          switch(Math.round(score)) {
+            case 9:
+              document.querySelector('.score-90').classList.remove('hide')
+              break
+            case 8:
+            case 7:
+              document.querySelector('.score-80').classList.remove('hide')
+              break
+            case 6:
+            case 5:
+              document.querySelector('.score-60').classList.remove('hide')
+              break
+            default:
+              document.querySelector('.score-00').classList.remove('hide')
+
+          }
+        }
+
+      })
     
     
 
