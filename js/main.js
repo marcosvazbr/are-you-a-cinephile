1384b27d30ba19ea32bd36109d4c4f7952da68c4
marcosvazbr <mvg.vaz@gmail.com>
2023-03-27 18:50:50 -0300

    evoluindo

--- js/main.js
+++ js/main.js
@@ -32,16 +32,51 @@ function movieApi(name) {
   })
 }
 
-    function listConstructor(listOfFilms) {
+    function listConstructor(listOfOptions) {
       document.querySelector('ul').innerHTML = " "
-      listOfFilms.forEach(element => {
+      listOfOptions.forEach(element => {
         let eachFilme = document.createElement('li')
   
         eachFilme.innerHTML = `<li class="option"><img src="https://image.tmdb.org/t/p/w500/${element.poster_path}" alt="filme"> <span>${element.title}</span>  </li>`
         
         document.querySelector('ul').append(eachFilme) 
-      })
+
+        eachFilme.addEventListener('click', function (){
+          listOfFilmes(element)
+          document.querySelector('ul').innerHTML = " "
+          inputSearch.value = ""
+        })
+      }) 
     }
+
+    function listOfFilmes(filme) {
+        console.log('chegou aqui')
+        console.log(filme)
+
+        
+        let filmTr = document.createElement('tr')
+        filmTr.innerHTML = `<td id="name-movie">
+        <div id="img-movie">
+          <img src="https://image.tmdb.org/t/p/w500/${filme.poster_path}" alt="Capa do filme">
+        </div>
+        <div>
+          <a href="https://www.themoviedb.org/movie/${filme.id}" target="_blank">${filme.original_title} 
+            <p>${filme.title}</p>
+          </a>
+          
+        </div>
+      </td> 
+      <td id="typeMovie">${filme.release_date}</td>
+          <td id="yearMovie">${filme.vote_count}</td>
+          <td id="scoreMovie">${filme.vote_average}</td>`
+        
+
+          document.querySelector('tbody').append(filmTr)
+        
+
+      }
+    
+    
 
 
 
