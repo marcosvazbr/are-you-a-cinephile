a1a58371f80b1254184924dad7d19ec13e420514
marcosvazbr <mvg.vaz@gmail.com>
2023-03-30 15:20:43 -0300

    limitando quantidades de opções

--- js/list-of-options.js
+++ js/list-of-options.js
@@ -7,7 +7,8 @@ export function movieApi(name, ) {
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=051e015fad5371e8aa5efffc2cf78b4b&query=${name}&language=pt-BR`)
   .then((response) => response.json())
   .then(data => {
-    for (let i=0; i<=data.results.length -1; i++) {
+    let numberOptions = data.results.length <= 5 ? data.results.length - 1 : 5
+    for (let i=0; i <= numberOptions; i++) {
       films.push(data.results[i])
       }
     listConstructor(films)
